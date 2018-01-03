d719dce3b1b1ecf9735babbf6c9ad1b195db75fd
AGuerrav <aaguerravaldes@gmail.com>
2018-01-03 17:48:48 -0300

    js, profile, img.profile

---
+++ assets/js/newpost.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+})
+var idPost = 0;
+function newPost(){
+  var title = $('#newTitle').val(); //variable para rescatar el contenido del titulo ingresado por el usuario
+  var urlImg = $('newImg').val(); //variable para rescatar la url de la imagen  ingresada por el usuario
+  var Opinion = $('newOpinion').val(); //variable para rescatar la opinion del usuario
+  //creando el contenedor de mi nuevo post
+  $('#my-opinion').append("<div id='post_" + idPost + "' class='pin  col-lg-6 col-md-6 col-xs-12 col-sm-12'>") +
+  
+
+
+}
